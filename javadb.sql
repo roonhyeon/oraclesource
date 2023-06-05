5c39d8a404eb15159480cf33232ec15589fd2764
Sue <roonhyeon@gmail.com>
2023-06-05 18:13:53 +0900

    auth table created

--- javadb.sql
+++ javadb.sql
@@ -445,9 +445,21 @@ last_used timestamp not null);
 
 --22. spring_board 연결할 user 테이블 생성 => spring_member
 --userid, userpw, username(성명), regdate, updatedate, enabled
+create table spring_member(
+userid varchar2(50) primary key,
+userpw varchar2(100) not null,
+username varchar2(100) not null,
+regdate date default sysdate,
+updatedate date default sysdate,
+enabled char(1) default '1');
 
 
-
+--23. spring_member 권한 테이블 생성 => spring_member_auth
+--userid, auth
+create table spring_member_auth(
+userid varchar2(50) not null,
+auth varchar2(50) not null,
+constraint fk_member_auth foreign key(userid) references spring_member(userid));
 
 
 
